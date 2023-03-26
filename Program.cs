7cefccc75a533cb5e504d9c9a0f35ea2063d3637
RecepPekdas <receppekdas3460@gmail.com>
2023-03-26 12:37:43 +0300

    fix: add forgotten inputs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hesap Makinesi\n");
             Console.Write("Birinci Sayıyı Giriniz: ");
             int sayi1 = int.Parse(Console.ReadLine());
+            Console.Write("İşlem Seçiniz(+,-,/,*): ");
+            string islem = Console.ReadLine();
             Console.Write("İkinci Sayıyı Giriniz: ");
             int sayi2 = int.Parse(Console.ReadLine());
         }
