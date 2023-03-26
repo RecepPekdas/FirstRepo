f620e9b8a63feb0abea0e859d61c35495a9ae0ca
RecepPekdas <receppekdas3460@gmail.com>
2023-03-26 12:43:06 +0300

    feat: sum function

--- Program.cs
+++ Program.cs
@@ -7,10 +7,18 @@
             Console.WriteLine("Hesap Makinesi\n");
             Console.Write("Birinci Sayıyı Giriniz: ");
             int sayi1 = int.Parse(Console.ReadLine());
-            Console.Write("İşlem Seçiniz(+,-,/,*): ");
+            Console.Write("İşlem Seçiniz(+) (-) (/) (*) (%): ");
             string islem = Console.ReadLine();
             Console.Write("İkinci Sayıyı Giriniz: ");
             int sayi2 = int.Parse(Console.ReadLine());
+            int sonuc = 0;
+            switch (islem)
+            {
+                case "+":
+                    sonuc = sayi1 + sayi2;
+                    break;
+            }
+            Console.WriteLine("{0} {1} {2} = {3}", sayi1, islem, sayi2, sonuc);
         }
     }
 }
