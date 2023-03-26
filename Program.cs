79c036533a63680f2cf42ef7b22967454b9032da
RecepPekdas <receppekdas3460@gmail.com>
2023-03-26 12:45:26 +0300

    feat: add multiply function

--- Program.cs
+++ Program.cs
@@ -20,6 +20,9 @@
                 case "-":
                     sonuc = sayi1 - sayi2;
                     break;
+                case "*":
+                    sonuc = sayi1 * sayi2;
+                    break;
             }
             Console.WriteLine("{0} {1} {2} = {3}", sayi1, islem, sayi2, sonuc);
         }
